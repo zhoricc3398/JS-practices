467f77d0b46709eefb54e35edebbd1c8ec0dbfee
zhoricc3398 <16200126@ibsu.edu.ge>
2019-06-07 21:02:21 +0400

    fix(11-Object-Oriented-Programming-Part-2): Fix practice2

--- 11-Object-Oriented-Programming-Part-2/practice2.js
+++ 11-Object-Oriented-Programming-Part-2/practice2.js
@@ -1,21 +1,23 @@
 {
     let waterAmount = 0;
-    let getTimeToBoil;
 
     function CoffeeMachine(power) {
         waterAmount = 0;
-        var WATER_HEAT_CAPACITY = 4200;
+        this.power = power;
 
         getTimeToBoil = function() {
             return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
         }
 
     }
+    CoffeeMachine.prototype.getTimeToBoil = function() {
+        return waterAmount * this.WATER_HEAT_CAPACITY * 80 / this.power;
+    };
     CoffeeMachine.prototype.run = function () {
         setTimeout(function () {
             console.log('Coffee is ready!');
-        }, getTimeToBoil());
-    }
+        }, this.getTimeToBoil());
+    };
     CoffeeMachine.prototype.setWaterAmount = function (amount) {
         waterAmount = amount;
     };
